bbc277e7298c1c6a0998f1983e887ac8baee5395
Niquelle J <41593517+NiqJohnson1@users.noreply.github.com>
2021-06-22 23:29:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const number = 16
+// print out from 1 up to and including the number
+// if the number is divisible by 3, print "Fizz"
+// if the number is divisible by 5, print "Buzz"
+// if the number is divisible by 3 and 5 print "FizzBuzz"
+// otherwise prints the number
+
